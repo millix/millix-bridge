6656acbf203a7cddc6e2ec7af7a8f02eebdad904
João Almeida <joao.rafael.almeida@ua.pt>
2022-11-25 00:53:31 +0000

    add database connection and transaction model

---
+++ core/storage/models/transaction.model.js
@@ -0,0 +1,54 @@
+import {DataTypes} from 'sequelize';
+import Database from '../database';
+
+const sequelize = Database.getConnection();
+
+const Transaction = sequelize.define('transaction', {
+    id               : {
+        type        : DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey  : true
+    },
+    networkFrom      : {
+        type     : DataTypes.STRING,
+        allowNull: false
+    },
+    networkTo        : {
+        type     : DataTypes.STRING,
+        allowNull: false
+    },
+    transactionIdFrom: {
+        type     : DataTypes.STRING,
+        allowNull: false
+    },
+    transactionIdTo  : {
+        type     : DataTypes.STRING,
+        allowNull: false
+    },
+    addressFrom      : {
+        type     : DataTypes.STRING,
+        allowNull: false
+    },
+    addressTo        : {
+        type     : DataTypes.STRING,
+        allowNull: false
+    },
+    amount           : {
+        type     : DataTypes.BIGINT,
+        allowNull: false
+    },
+    event            : {
+        type     : DataTypes.STRING,
+        allowNull: false
+    },
+    processingState  : {
+        type     : DataTypes.STRING,
+        allowNull: false
+    },
+    status           : {
+        type     : DataTypes.SMALLINT,
+        allowNull: false
+    }
+});
+
+export default Transaction;
